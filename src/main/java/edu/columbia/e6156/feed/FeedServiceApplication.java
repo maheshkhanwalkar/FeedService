e778af0a3cc8609653e0a5f73ffe7fe69817349a
Mahesh Khanwalkar <maheshkhanwalkar@gmail.com>
2022-12-04 13:32:30 -0500

    Set up Spring Boot application

---
+++ src/main/java/edu/columbia/e6156/feed/FeedServiceApplication.java
@@ -0,0 +1,15 @@
+package edu.columbia.e6156.feed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeedServiceApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(FeedServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/columbia/e6156/feed/FeedServiceApplicationTests.java
@@ -0,0 +1,15 @@
+package edu.columbia.e6156.feed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeedServiceApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
